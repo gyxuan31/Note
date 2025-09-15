3142890f8676cdb1755feea7f0e98c7e0f03e3e0
gyxuan31 <gyxuan0131@gmail.com>
2025-09-15 11:06:00 -0400

    update

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        cnt = 0
         tgrid = np.array(grid).T
         tgrid = tgrid.tolist()
-        for i, column in enumerate(tgrid):
-            if column == grid[i]:
-                return i
-        return 0
+        for column in tgrid:
+            if column in grid:
+                cnt += 1
+        return cnt
 a = Solution()
 print(a.equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
