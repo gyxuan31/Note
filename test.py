f01ff4453b3c14de051266a23728513fd5794eb6
gyxuan31 <gyxuan0131@gmail.com>
2025-09-24 11:07:58 -0400

    update

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
-import numpy as np
+from collections import deque
 class Solution(object):
-    def equalPairs(self, grid):
+    def predictPartyVictory(self, senate):
         """
-        :type grid: List[List[int]]
-        :rtype: int
+        :type senate: str
+        :rtype: str
         """
-        cnt = 0
-        tgrid = np.array(grid).T
-        tgrid = tgrid.tolist()
-        for column in tgrid:
-            if column in grid:
-                cnt += 1
-        return cnt
+        n_r = 0
+        n_d = 0
+        q = deque(senate)
+
+        while len(q) > 1 or (('R' in q) and ('D' in q)):
+            q_new = deque()
+            for c in q:
+                if n_r > 0 and c == 'R':
+                    n_r -= 1
+                    continue
+                elif n_d > 0 and c == 'D':
+                    n_d -= 1
+                    continue
+                elif c == 'R':
+                    n_d += 1
+                    q_new.append(c)
+                else:
+                    n_r += 1
+                    q_new.append(c)
+            q = q_new
+        if q[0] == 'R':
+            return 'Radiant'
+        else:
+            return 'Dire'
 a = Solution()
-print(a.equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
+print(a.predictPartyVictory("RRR"))
